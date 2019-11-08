0c98e3faa7781ed2b16557068bb9a3a3faf9dd3b
HIRAKO Jun <uk6057965@ssh.ice.nuie.nagoya-u.ac.jp>
2019-11-08 10:52:37 +0900

    motor

--- motor.c
+++ motor.c
@@ -13,12 +13,33 @@
 #define GPIO0 (*(volatile unsigned int *) 0x0320)
 
 void my_motor();
+void my_motor_re();
 void ext_out(); 
+void my_scan();
+void my_print();
+
+
 
 main() {
-  while(1){
+  unsigned int command[16];
+  unsigned int input_str[16];
+
+
+  input_str[0] = 'N';  input_str[1] = 'E';
+  input_str[2] = 'X';  input_str[3] = 'T';
+  input_str[4] = '=';  input_str[5] = '\0';
+  my_print(input_str);
+
+  my_scan(command);
+  if (command[0] == 'R') {
+      while(TRUE){
+        my_motor_re();
+      }
+  }
+
+  while(TRUE){
     my_motor();
-  } 
+  }
 } 
  
 void my_motor() {
@@ -27,9 +48,114 @@ void my_motor() {
   ext_out(2); 
   ext_out(1); 
 } 
+
+void my_motor_re() {
+  ext_out(1); 
+  ext_out(2); 
+  ext_out(4); 
+  ext_out(8); 
+}
  
 void ext_out(unsigned int num) {
   unsigned int i;
 
   GPIO0 = num; 
+}
+
+/* キーボードから入力された文字列を str[] に記憶する関数 */
+void my_scan(str)
+     unsigned int *str;
+{
+    EXTIO_SCAN_STROKE = (unsigned int)0x00000000;
+    EXTIO_SCAN_REQ = (unsigned int)0x00000001;
+    EXTIO_SCAN_STROKE = (unsigned int)0x00000001;
+
+    EXTIO_SCAN_STROKE = (unsigned int)0x00000000;
+    EXTIO_SCAN_STROKE = (unsigned int)0x00000001;
+    while (EXTIO_SCAN_ASCII == SCAN_STRORING) {
+      EXTIO_SCAN_STROKE = (unsigned int)0x00000000;
+      EXTIO_SCAN_STROKE = (unsigned int)0x00000001;
+    }
+
+    while ((*str = EXTIO_SCAN_ASCII) != (unsigned int)0x3e) {    // 0x3e=RETURN
+      if ((*str >= 1) && (*str <= 26)) {
+	*str = 'A' + *str - 1;
+      } else if ((*str >= 48) && (*str <= 57)) {
+	*str = '0' + *str - 48;
+      } else {
+	if (*str == 0) {
+	  *str = '@';
+	} else if (*str == 27) {
+	  *str = '[';
+	} else if (*str == 29) {
+	  *str = ']';
+	} else if ((*str >= 32) && (*str <= 47)) {
+	  *str = ' ' + *str - 32;
+	} else if (*str == 58) {
+	  *str = '?';
+	} else if (*str == 59) {
+	  *str = '=';
+	} else if (*str == 60) {
+	  *str = ';';
+	} else if (*str == 61) {
+	  *str = ':';
+	} else if (*str == 62) {
+	  *str = '\n';
+	} else {
+	  *str = '@';
+	}
+      }
+      EXTIO_SCAN_STROKE = (unsigned int)0x00000000;
+      EXTIO_SCAN_STROKE = (unsigned int)0x00000001;
+      str++;
+    }
+    *str = '\0';
+
+    EXTIO_SCAN_STROKE = (unsigned int)0x00000000;
+    EXTIO_SCAN_REQ = (unsigned int)0x00000000;
+    EXTIO_SCAN_STROKE = (unsigned int)0x00000001;
+
+    EXTIO_SCAN_STROKE = (unsigned int)0x00000000;
+}
+
+/* 文字列 str[] を表示する関数 */
+void my_print(str)
+     unsigned int *str;
+{
+  while (*str != '\0') {
+    EXTIO_PRINT_STROKE = (unsigned int)0x00000000;
+
+    if ((*str >= 'A') && (*str <= 'Z')) {
+      EXTIO_PRINT_ASCII = *str - 'A' + 1;
+    } else if ((*str >= 'a') && (*str <= 'z')) {
+      EXTIO_PRINT_ASCII = *str - 'a' + 1;
+    } else if ((*str >= '0') && (*str <= '9')) {
+      EXTIO_PRINT_ASCII = *str - '0' + 48;
+    } else {
+      if (*str == '@') {
+        EXTIO_PRINT_ASCII = (unsigned int)0;
+      } else if (*str == '[') {
+        EXTIO_PRINT_ASCII = (unsigned int)27;
+      } else if (*str == ']') {
+        EXTIO_PRINT_ASCII = (unsigned int)29;
+      } else if ((*str >= ' ') && (*str <= '/')) {
+        EXTIO_PRINT_ASCII = *str - ' ' + 32;
+      } else if (*str == '?') {
+        EXTIO_PRINT_ASCII = (unsigned int)58;
+      } else if (*str == '=') {
+        EXTIO_PRINT_ASCII = (unsigned int)59;
+      } else if (*str == ';') {
+        EXTIO_PRINT_ASCII = (unsigned int)60;
+      } else if (*str == ':') {
+        EXTIO_PRINT_ASCII = (unsigned int)61;
+      } else if (*str == '\n') {
+	EXTIO_PRINT_ASCII = (unsigned int)62;
+      } else {
+        EXTIO_PRINT_ASCII = (unsigned int)0x00000000;
+      }
+    }
+   
+    EXTIO_PRINT_STROKE = (unsigned int)0x00000001;
+    str++;
+  }
 }
--- motor.s
+++ motor.s
@@ -6,16 +6,49 @@
 	.globl	main
 	.ent	main
 main:
-	.frame	$fp,24,$31		# vars= 0, regs= 2/0, args= 16, gp= 0
+	.frame	$fp,152,$31		# vars= 128, regs= 2/0, args= 16, gp= 0
 	.mask	0xc0000000,-4
 	.fmask	0x00000000,0
 	.set	noreorder
 	.set	nomacro
 	
-	addiu	$sp,$sp,-24
-	sw	$31,20($sp)
-	sw	$fp,16($sp)
+	addiu	$sp,$sp,-152
+	sw	$31,148($sp)
+	sw	$fp,144($sp)
 	move	$fp,$sp
+	li	$2,78			# 0x4e
+	sw	$2,80($fp)
+	li	$2,69			# 0x45
+	sw	$2,84($fp)
+	li	$2,88			# 0x58
+	sw	$2,88($fp)
+	li	$2,84			# 0x54
+	sw	$2,92($fp)
+	li	$2,61			# 0x3d
+	sw	$2,96($fp)
+	sw	$0,100($fp)
+	addiu	$2,$fp,80
+	move	$4,$2
+	jal	my_print
+	nop
+
+	addiu	$2,$fp,16
+	move	$4,$2
+	jal	my_scan
+	nop
+
+	lw	$3,16($fp)
+	li	$2,82			# 0x52
+	bne	$3,$2,$L2
+	nop
+
+$L3:
+	jal	my_motor_re
+	nop
+
+	j	$L3
+	nop
+
 $L2:
 	jal	my_motor
 	nop
@@ -69,6 +102,47 @@ my_motor:
 	.end	my_motor
 	.size	my_motor, .-my_motor
 	.align	2
+	.globl	my_motor_re
+	.ent	my_motor_re
+my_motor_re:
+	.frame	$fp,24,$31		# vars= 0, regs= 2/0, args= 16, gp= 0
+	.mask	0xc0000000,-4
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
+	
+	addiu	$sp,$sp,-24
+	sw	$31,20($sp)
+	sw	$fp,16($sp)
+	move	$fp,$sp
+	li	$4,1			# 0x1
+	jal	ext_out
+	nop
+
+	li	$4,2			# 0x2
+	jal	ext_out
+	nop
+
+	li	$4,4			# 0x4
+	jal	ext_out
+	nop
+
+	li	$4,8			# 0x8
+	jal	ext_out
+	nop
+
+	move	$sp,$fp
+	lw	$31,20($sp)
+	lw	$fp,16($sp)
+	addiu	$sp,$sp,24
+	j	$31
+	nop
+
+	.set	macro
+	.set	reorder
+	.end	my_motor_re
+	.size	my_motor_re, .-my_motor_re
+	.align	2
 	.globl	ext_out
 	.ent	ext_out
 ext_out:
@@ -96,4 +170,555 @@ ext_out:
 	.set	reorder
 	.end	ext_out
 	.size	ext_out, .-ext_out
+	.align	2
+	.globl	my_scan
+	.ent	my_scan
+my_scan:
+	.frame	$fp,8,$31		# vars= 0, regs= 1/0, args= 0, gp= 0
+	.mask	0x40000000,-8
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
+	
+	addiu	$sp,$sp,-8
+	sw	$fp,0($sp)
+	move	$fp,$sp
+	sw	$4,8($fp)
+	li	$2,776			# 0x308
+	sw	$0,0($2)
+	li	$3,780			# 0x30c
+	li	$2,1			# 0x1
+	sw	$2,0($3)
+	li	$3,776			# 0x308
+	li	$2,1			# 0x1
+	sw	$2,0($3)
+	li	$2,776			# 0x308
+	sw	$0,0($2)
+	li	$3,776			# 0x308
+	li	$2,1			# 0x1
+	sw	$2,0($3)
+	j	$L12
+	nop
+
+$L13:
+	li	$2,776			# 0x308
+	sw	$0,0($2)
+	li	$3,776			# 0x308
+	li	$2,1			# 0x1
+	sw	$2,0($3)
+$L12:
+	li	$2,784			# 0x310
+	lw	$3,0($2)
+	li	$2,-1			# 0xffffffffffffffff
+	beq	$3,$2,$L13
+	nop
+
+	j	$L15
+	nop
+
+$L16:
+	lw	$2,8($fp)
+	nop
+	lw	$2,0($2)
+	nop
+	beq	$2,$0,$L17
+	nop
+
+	lw	$2,8($fp)
+	nop
+	lw	$2,0($2)
+	nop
+	sltu	$2,$2,27
+	beq	$2,$0,$L17
+	nop
+
+	lw	$2,8($fp)
+	nop
+	lw	$2,0($2)
+	nop
+	addiu	$3,$2,64
+	lw	$2,8($fp)
+	nop
+	sw	$3,0($2)
+	j	$L20
+	nop
+
+$L17:
+	lw	$2,8($fp)
+	nop
+	lw	$2,0($2)
+	nop
+	sltu	$2,$2,48
+	bne	$2,$0,$L21
+	nop
+
+	lw	$2,8($fp)
+	nop
+	lw	$2,0($2)
+	nop
+	sltu	$2,$2,58
+	beq	$2,$0,$L21
+	nop
+
+	lw	$2,8($fp)
+	nop
+	lw	$3,0($2)
+	lw	$2,8($fp)
+	nop
+	sw	$3,0($2)
+	j	$L20
+	nop
+
+$L21:
+	lw	$2,8($fp)
+	nop
+	lw	$2,0($2)
+	nop
+	bne	$2,$0,$L24
+	nop
+
+	lw	$3,8($fp)
+	li	$2,64			# 0x40
+	sw	$2,0($3)
+	j	$L20
+	nop
+
+$L24:
+	lw	$2,8($fp)
+	nop
+	lw	$3,0($2)
+	li	$2,27			# 0x1b
+	bne	$3,$2,$L26
+	nop
+
+	lw	$3,8($fp)
+	li	$2,91			# 0x5b
+	sw	$2,0($3)
+	j	$L20
+	nop
+
+$L26:
+	lw	$2,8($fp)
+	nop
+	lw	$3,0($2)
+	li	$2,29			# 0x1d
+	bne	$3,$2,$L28
+	nop
+
+	lw	$3,8($fp)
+	li	$2,93			# 0x5d
+	sw	$2,0($3)
+	j	$L20
+	nop
+
+$L28:
+	lw	$2,8($fp)
+	nop
+	lw	$2,0($2)
+	nop
+	sltu	$2,$2,32
+	bne	$2,$0,$L30
+	nop
+
+	lw	$2,8($fp)
+	nop
+	lw	$2,0($2)
+	nop
+	sltu	$2,$2,48
+	beq	$2,$0,$L30
+	nop
+
+	lw	$2,8($fp)
+	nop
+	lw	$3,0($2)
+	lw	$2,8($fp)
+	nop
+	sw	$3,0($2)
+	j	$L20
+	nop
+
+$L30:
+	lw	$2,8($fp)
+	nop
+	lw	$3,0($2)
+	li	$2,58			# 0x3a
+	bne	$3,$2,$L33
+	nop
+
+	lw	$3,8($fp)
+	li	$2,63			# 0x3f
+	sw	$2,0($3)
+	j	$L20
+	nop
+
+$L33:
+	lw	$2,8($fp)
+	nop
+	lw	$3,0($2)
+	li	$2,59			# 0x3b
+	bne	$3,$2,$L35
+	nop
+
+	lw	$3,8($fp)
+	li	$2,61			# 0x3d
+	sw	$2,0($3)
+	j	$L20
+	nop
+
+$L35:
+	lw	$2,8($fp)
+	nop
+	lw	$3,0($2)
+	li	$2,60			# 0x3c
+	bne	$3,$2,$L37
+	nop
+
+	lw	$3,8($fp)
+	li	$2,59			# 0x3b
+	sw	$2,0($3)
+	j	$L20
+	nop
+
+$L37:
+	lw	$2,8($fp)
+	nop
+	lw	$3,0($2)
+	li	$2,61			# 0x3d
+	bne	$3,$2,$L39
+	nop
+
+	lw	$3,8($fp)
+	li	$2,58			# 0x3a
+	sw	$2,0($3)
+	j	$L20
+	nop
+
+$L39:
+	lw	$2,8($fp)
+	nop
+	lw	$3,0($2)
+	li	$2,62			# 0x3e
+	bne	$3,$2,$L41
+	nop
+
+	lw	$3,8($fp)
+	li	$2,10			# 0xa
+	sw	$2,0($3)
+	j	$L20
+	nop
+
+$L41:
+	lw	$3,8($fp)
+	li	$2,64			# 0x40
+	sw	$2,0($3)
+$L20:
+	li	$2,776			# 0x308
+	sw	$0,0($2)
+	li	$3,776			# 0x308
+	li	$2,1			# 0x1
+	sw	$2,0($3)
+	lw	$2,8($fp)
+	nop
+	addiu	$2,$2,4
+	sw	$2,8($fp)
+$L15:
+	li	$2,784			# 0x310
+	lw	$3,0($2)
+	lw	$2,8($fp)
+	nop
+	sw	$3,0($2)
+	lw	$2,8($fp)
+	nop
+	lw	$3,0($2)
+	li	$2,62			# 0x3e
+	bne	$3,$2,$L16
+	nop
+
+	lw	$2,8($fp)
+	nop
+	sw	$0,0($2)
+	li	$2,776			# 0x308
+	sw	$0,0($2)
+	li	$2,780			# 0x30c
+	sw	$0,0($2)
+	li	$3,776			# 0x308
+	li	$2,1			# 0x1
+	sw	$2,0($3)
+	li	$2,776			# 0x308
+	sw	$0,0($2)
+	move	$sp,$fp
+	lw	$fp,0($sp)
+	addiu	$sp,$sp,8
+	j	$31
+	nop
+
+	.set	macro
+	.set	reorder
+	.end	my_scan
+	.size	my_scan, .-my_scan
+	.align	2
+	.globl	my_print
+	.ent	my_print
+my_print:
+	.frame	$fp,8,$31		# vars= 0, regs= 1/0, args= 0, gp= 0
+	.mask	0x40000000,-8
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
+	
+	addiu	$sp,$sp,-8
+	sw	$fp,0($sp)
+	move	$fp,$sp
+	sw	$4,8($fp)
+	j	$L46
+	nop
+
+$L47:
+	li	$2,768			# 0x300
+	sw	$0,0($2)
+	lw	$2,8($fp)
+	nop
+	lw	$2,0($2)
+	nop
+	sltu	$2,$2,65
+	bne	$2,$0,$L48
+	nop
+
+	lw	$2,8($fp)
+	nop
+	lw	$2,0($2)
+	nop
+	sltu	$2,$2,91
+	beq	$2,$0,$L48
+	nop
+
+	li	$3,772			# 0x304
+	lw	$2,8($fp)
+	nop
+	lw	$2,0($2)
+	nop
+	addiu	$2,$2,-64
+	sw	$2,0($3)
+	j	$L51
+	nop
+
+$L48:
+	lw	$2,8($fp)
+	nop
+	lw	$2,0($2)
+	nop
+	sltu	$2,$2,97
+	bne	$2,$0,$L52
+	nop
+
+	lw	$2,8($fp)
+	nop
+	lw	$2,0($2)
+	nop
+	sltu	$2,$2,123
+	beq	$2,$0,$L52
+	nop
+
+	li	$3,772			# 0x304
+	lw	$2,8($fp)
+	nop
+	lw	$2,0($2)
+	nop
+	addiu	$2,$2,-96
+	sw	$2,0($3)
+	j	$L51
+	nop
+
+$L52:
+	lw	$2,8($fp)
+	nop
+	lw	$2,0($2)
+	nop
+	sltu	$2,$2,48
+	bne	$2,$0,$L55
+	nop
+
+	lw	$2,8($fp)
+	nop
+	lw	$2,0($2)
+	nop
+	sltu	$2,$2,58
+	beq	$2,$0,$L55
+	nop
+
+	li	$2,772			# 0x304
+	lw	$3,8($fp)
+	nop
+	lw	$3,0($3)
+	nop
+	sw	$3,0($2)
+	j	$L51
+	nop
+
+$L55:
+	lw	$2,8($fp)
+	nop
+	lw	$3,0($2)
+	li	$2,64			# 0x40
+	bne	$3,$2,$L58
+	nop
+
+	li	$2,772			# 0x304
+	sw	$0,0($2)
+	j	$L51
+	nop
+
+$L58:
+	lw	$2,8($fp)
+	nop
+	lw	$3,0($2)
+	li	$2,91			# 0x5b
+	bne	$3,$2,$L60
+	nop
+
+	li	$3,772			# 0x304
+	li	$2,27			# 0x1b
+	sw	$2,0($3)
+	j	$L51
+	nop
+
+$L60:
+	lw	$2,8($fp)
+	nop
+	lw	$3,0($2)
+	li	$2,93			# 0x5d
+	bne	$3,$2,$L62
+	nop
+
+	li	$3,772			# 0x304
+	li	$2,29			# 0x1d
+	sw	$2,0($3)
+	j	$L51
+	nop
+
+$L62:
+	lw	$2,8($fp)
+	nop
+	lw	$2,0($2)
+	nop
+	sltu	$2,$2,32
+	bne	$2,$0,$L64
+	nop
+
+	lw	$2,8($fp)
+	nop
+	lw	$2,0($2)
+	nop
+	sltu	$2,$2,48
+	beq	$2,$0,$L64
+	nop
+
+	li	$2,772			# 0x304
+	lw	$3,8($fp)
+	nop
+	lw	$3,0($3)
+	nop
+	sw	$3,0($2)
+	j	$L51
+	nop
+
+$L64:
+	lw	$2,8($fp)
+	nop
+	lw	$3,0($2)
+	li	$2,63			# 0x3f
+	bne	$3,$2,$L67
+	nop
+
+	li	$3,772			# 0x304
+	li	$2,58			# 0x3a
+	sw	$2,0($3)
+	j	$L51
+	nop
+
+$L67:
+	lw	$2,8($fp)
+	nop
+	lw	$3,0($2)
+	li	$2,61			# 0x3d
+	bne	$3,$2,$L69
+	nop
+
+	li	$3,772			# 0x304
+	li	$2,59			# 0x3b
+	sw	$2,0($3)
+	j	$L51
+	nop
+
+$L69:
+	lw	$2,8($fp)
+	nop
+	lw	$3,0($2)
+	li	$2,59			# 0x3b
+	bne	$3,$2,$L71
+	nop
+
+	li	$3,772			# 0x304
+	li	$2,60			# 0x3c
+	sw	$2,0($3)
+	j	$L51
+	nop
+
+$L71:
+	lw	$2,8($fp)
+	nop
+	lw	$3,0($2)
+	li	$2,58			# 0x3a
+	bne	$3,$2,$L73
+	nop
+
+	li	$3,772			# 0x304
+	li	$2,61			# 0x3d
+	sw	$2,0($3)
+	j	$L51
+	nop
+
+$L73:
+	lw	$2,8($fp)
+	nop
+	lw	$3,0($2)
+	li	$2,10			# 0xa
+	bne	$3,$2,$L75
+	nop
+
+	li	$3,772			# 0x304
+	li	$2,62			# 0x3e
+	sw	$2,0($3)
+	j	$L51
+	nop
+
+$L75:
+	li	$2,772			# 0x304
+	sw	$0,0($2)
+$L51:
+	li	$3,768			# 0x300
+	li	$2,1			# 0x1
+	sw	$2,0($3)
+	lw	$2,8($fp)
+	nop
+	addiu	$2,$2,4
+	sw	$2,8($fp)
+$L46:
+	lw	$2,8($fp)
+	nop
+	lw	$2,0($2)
+	nop
+	bne	$2,$0,$L47
+	nop
+
+	move	$sp,$fp
+	lw	$fp,0($sp)
+	addiu	$sp,$sp,8
+	j	$31
+	nop
+
+	.set	macro
+	.set	reorder
+	.end	my_print
+	.size	my_print, .-my_print
 	.ident	"GCC: (GNU) 4.2.0"
